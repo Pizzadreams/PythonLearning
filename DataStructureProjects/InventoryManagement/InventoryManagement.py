4c492d3320c4627dd3a3821e61a036cea01ac787
Andrew <retsim120@gmail.com>
2023-06-24 01:28:41 -0400

    updated code to clear terminal, and added some validate user input for the option

--- DataStructureProjects/InventoryManagement/InventoryManagement.py
+++ DataStructureProjects/InventoryManagement/InventoryManagement.py
@@ -1,24 +1,68 @@
 # Develop an inventory management system where you can keep track of available items and their quantities.
+import os 
+
 
 print("Salutations. I am VexIM, your Inventory Management program.") # Validate Encrypt eXamine Inventory Management (WIP)
 itemsDict = {}
 
 def view_items(items_dict):
-    print("Here is your inventory:")
-    for item, quantity in items_dict.items():
-        print(f"{item}: {quantity}")
+    if not items_dict:
+        print("The inventory is empty.")
+    else:
+      print("Here is your inventory:")
+      for item, quantity in items_dict.items():
+          print(f"\t{item}: {quantity}")
+
+def validate_item_input(item):
+    if not item.strip():
+        print("Invalid input. Please enter a valid item name.")
+        return False
+    return True
+
+def validate_quantity_input(quantity):
+    if quantity <= 0:
+        print("Invalid input. Please enter a positive quantity.")
+        return False
+    return True
+
+def validate_option_input(input, allowed_options):
+    if input.lower() not in allowed_options:
+        print("Invalid input. Please enter a valid option.")
+        wait_for_enter()
+        return False
+    return True
+
+def wait_for_enter():
+    input("Press Enter to continue...")
 
 while True:
+    os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
     # option variable is used to store the user's input 
-    option = input("Enter 'add' to add an item, 'view' to view the inventory, or 'quit' to exit: ").lower() #.lower() converts input to lowercase before performing comparisons to ensure they are not case sensitive
+    option = input("Enter 'add' to add an item, 'view' to view the inventory, or 'quit' to exit: ").lower()
+    allowed_options = ['add', 'view', 'quit']
+    if not validate_option_input(option, allowed_options):
+      continue
     if option == 'quit':
         print("Thank you for using VexIM.")
         break
     elif option == 'add':
-        item = input("Please tell me what you would like to add to your inventory: ")
-        itemsDict.setdefault(item, 0)
-        itemsDict[item] += 1
+      item = input("What would you like to add to your inventory: ").lower()
+      if not validate_item_input(item):
+          # wait_for_enter()
+          continue
+
+      quantity = int(input("Please enter the quantity to add: "))
+      if not validate_quantity_input(quantity):
+          # wait_for_enter()   
+          continue
+
+      itemsDict.setdefault(item, 0)
+      itemsDict[item] += quantity
+      print(f"You have added {quantity} {item}{'s' if quantity > 1 else ''}. Total count: {itemsDict[item]}")
+      input("Press Enter to continue...")
+
     elif option == 'view':
         view_items(itemsDict)
+        input("Press Enter to continue...")
     else:
         print("Invalid option. Please try again.")
