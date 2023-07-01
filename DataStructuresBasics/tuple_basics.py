d15cf3109c69835cfd56500e4b1b4d11d2923b9b
Andrew <retsim120@gmail.com>
2023-07-01 00:28:05 -0400

    transposed output of animals and car methods with .zip

--- DataStructuresBasics/tuple_basics.py
+++ DataStructuresBasics/tuple_basics.py
@@ -10,14 +10,21 @@ def display_animals():
         ("Tiger", "Female", 4),
     )
     
+    transposed_animals = zip(*animals)
+
     print("'Type' of animals:", type(animals).__name__)
-    
-    for animal in animals:
-        species, sex, age = animal
-        print("Species:", species)
-        print("Sex:", sex)
-        print("Age:", age)
-        print()
+
+    # Display animals side by side
+    for animal in transposed_animals: # iterate over each tuple 
+        animal_info = " | ".join(str(info).ljust(8) for info in animal) # convert each item in the 'animal' tuple to a string, and separate them by the '|'
+        print(animal_info)
+    print()
+    # for animal in animals:
+    #     species, sex, age in animal
+    #     print("Species:", species)
+    #     print("Sex:", sex)
+    #     print("Age:", age)
+    #     print()
 
 def display_cars():
     cars = (
@@ -29,11 +36,18 @@ def display_cars():
 
     print("'Type' of cars:", type(cars).__name__)
 
-    for make, model, year in cars:
-        print("Make:", make)
-        print("Model:", model)
-        print("Year:", year)
-        print()
+    makes, models, years = zip(*cars)
+
+    print("Make:", ", ".join(makes))
+    print("Model:", ", ".join(models))
+    print("Year:", ", ".join(map(str, years)))
+    print()
+
+    # for make, model, year in cars:
+    #     print("Make:", make)
+    #     print("Model:", model)
+    #     print("Year:", year)
+    #     print()
 
 
 display_animals()
