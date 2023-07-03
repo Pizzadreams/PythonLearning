d98f70a52ccc1da1d0cb9f38ed153a444bca0e16
Andrew <retsim120@gmail.com>
2023-07-03 18:29:35 -0400

    updated person.py with Student subclass, practicing Inheritance

--- OOP/main.py
+++ OOP/main.py
@@ -1,4 +1,8 @@
-from person import Person
+from person import Person, Student
 
 person1 = Person("Benny", 482)
 person1.introduce()
+
+student1 = Student("Juniper", 4, "117")
+student1.introduce()
+student1.study()
--- OOP/person.py
+++ OOP/person.py
@@ -8,7 +8,7 @@
 
 class Person:
     # __init__() ensures that the object's attributes are properly initialized when the object is created
-    def __init__(self, name, age): 
+    def __init__(self, name, age): # self represents the instance of the class
         # assign the value of the parameters to their attributes of the object
         self.name = name
         self.age = age
@@ -17,5 +17,19 @@ class Person:
         print(f"Hey. My name is {self.name} and I am {self.age} years old.")
 
 
+# person1 object created
 # person1 = Person("Benny", 482)
 # person1.introduce()
+
+
+"""
+Creating a sub-class of 'Person'. 'Student' will inherit from 'Person'.
+Student inherits all the attributes and methods of the 'Person' class.
+"""
+class Student(Person):
+    def __init__(self, name, age, student_id): 
+        super().__init__(name, age) # Initialzes the name and age from the parent class 'Person'
+        self.student_id = student_id # student_id is specific to the Student class
+
+    def study(self): # 'study' method is defined to display that the student is studying
+        print(f"Student {self.name} is studying.")
