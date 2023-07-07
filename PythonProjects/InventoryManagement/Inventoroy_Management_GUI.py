4635da811c72a15d45796a45fc92d89e0efdacde
Andrew <retsim120@gmail.com>
2023-07-07 17:17:28 -0400

    refactored methods and added new methods to display and hide labels based on the function

--- PythonProjects/InventoryManagement/Inventoroy_Management_GUI.py
+++ PythonProjects/InventoryManagement/Inventoroy_Management_GUI.py
@@ -8,28 +8,17 @@ root.title("VexIM - Inventory Management Program")
 
 items_dict = {}
 
-def validate_option_input(user_input, allowed_options):
-    while True:
-        if user_input.lower() in allowed_options:
-            return user_input.lower()
-        update_display("Invalid input. Please enter a valid option.")
-        user_input = input("Enter 'add' to add an item, 'view' to view the inventory, or 'quit' to exit: ")
+def hide_welcome_label():
+    label.pack_forget()
 
-def validate_item_input():
-  while True:
-    item = input("Please enter a valid item name: ")
-    if item.strip().isalpha():
-        return item.lower()
-    update_display("Invalid input. Please enter a valid item name.")
+def hide_option_and_quantity_fields():
+    option_entry.pack_forget()  # Hide the option_entry field
+    quantity_entry.pack_forget()  # Hide the quantity_entry field
 
-def validate_quantity_input():
-    while True:
-        quantity = input("Please enter the quantity to add: ")
-        if quantity.strip().isdigit() and int(quantity) > 0:
-            return int(quantity)
-        update_display("Invalid input. Please enter a positive quantity.")
+def view_items():
+    hide_welcome_label()
+    hide_option_and_quantity_fields()
 
-def view_items(items_dict):
     if not items_dict:
         update_display("The inventory is empty.")
     else:
@@ -37,17 +26,25 @@ def view_items(items_dict):
         for item, quantity in items_dict.items():
           update_display(f"\t{item.capitalize()}: {quantity}")
 
-def add_item(items_dict):
+"""Add item(s) to the inventory with the help of validation methods for item and quantity."""
+def add_item():
+    hide_welcome_label()
+    option_entry.pack()
     item = validate_item_input()
     quantity = validate_quantity_input()
-    items_dict.setdefault(item, 0)
-    items_dict[item] += int(quantity)
-    update_display(f"You have added {quantity} {item.capitalize()}{'(s)'}. Total count: {items_dict[item]}")
+    quantity_entry.pack()
     # update_display(f"You have added {quantity} {item.capitalize()}{'(s)'}. Total count: {items_dict[item]}")
-
+    
+    if quantity is not None:
+        items_dict.setdefault(item, 0)
+        items_dict[item] += quantity
+        update_display(f"You have added {quantity} {item.capitalize()}{'(s)'}. Total count: {items_dict[item]}")
 
 """Removes item(s) from the inventory with help of validate_quantity_to_remove()"""
-def remove_item(items_dict):
+def remove_item():
+    hide_welcome_label()
+    # TODO
+
     item = validate_item_input()
     if item in items_dict:
         quantity_in_inventory = items_dict[item] # retrieve the current quantity of the item from the dictionary
@@ -65,61 +62,74 @@ def remove_item(items_dict):
     else:
         update_display(f"{item.capitalize()} does not exist in the inventory.")
 
-def validate_quantity_to_remove(max_quantity):
-    """Validate the quantity to remove from an item when using the 'remove_item()'."""
+def validate_option_input(user_input, allowed_options):
     while True:
-        quantity = input(f"Please enter the quantity to remove (1 to {max_quantity}): ") 
-        if quantity.isdigit() and 1 <= int(quantity) <= max_quantity:
-            return int(quantity)
-        update("Invalid input. Please enter a valid quantity.")
+        if user_input.lower() in allowed_options:
+            return user_input.lower()
+        update_display("Invalid input. Please enter a valid option.")
+
+def validate_item_input():
+    item = option_entry.get().strip()
+    if item.strip().isalpha():
+        return item.lower()
+    update_display("Invalid input. Please enter a valid item name.")
+
+def validate_quantity_input():
+    quantity = quantity_entry.get().strip()
+    if quantity.isdigit() and int(quantity) > 0:
+        return int(quantity)
+    else:
+        update_display("Invalid input. Please enter a positive quantity.")
+        return None
+
+def validate_quantity_to_remove(max_quantity):
+    # Entry field for quantity
+    quantity_entry.pack()
+    """Validate the quantity to remove from an item when using the 'remove_item()'."""
+    quantity = quantity_entry.get()  # Get the value from the quantity_entry field
+    quantity_entry.delete(0, tk.END)  # Clear the quantity_entry field
+    if quantity.isdigit() and 1 <= int(quantity) <= max_quantity:
+        return int(quantity)
+    update_display("Invalid input. Please enter a valid quantity.")
 
 # Function to update the display label with the provided text
 def update_display(text):
     display_label.config(text = text)
 
-# Function to handle button click events
-def handle_button_click():
-    option = option_entry.get().strip().lower()
-    if option == 'add':
-        add_item()
-    elif option == 'remove':
-        remove_item()
-    elif option == 'view':
-        view_items()
-    elif option == 'quit':
-        root.quit()
-    else:
-        update_display("Invalid option. Please try again.")
+def quit_program():
+    hide_welcome_label()
+    hide_option_and_quantity_fields()
+    update_display("Thank you for using VexIM")
+    root.after(2000, root.destroy)
 
+quantity_entry = tk.Entry(root)
+option_entry = tk.Entry(root)
 
-# Create and pack the widgets in the GUI
 display_label = tk.Label(root, text="Salutations. I am VexIM, your Inventory Management program.")
 display_label.pack()
 
-option_label = tk.Label(root, text="Enter 'add' to add an item, 'remove' to remove an item, 'view' to view the inventory, or 'quit' to exit:")
-option_label.pack()
+# Label for instructions with options
+label = tk.Label(root, text="Welcome to VexIM. Please select an option:")
+label.pack()
 
-option_entry = tk.Entry(root)
-option_entry.pack()
+# Button for adding an item
+add_button = tk.Button(root, text="Add", command=add_item)
+add_button.pack()
 
-quantity_label = tk.Label(root, text="Please enter the quantity to add:")
-quantity_label.pack()
+# Button for removing an item
+remove_button = tk.Button(root, text="Remove", command=remove_item)
+remove_button.pack()
 
-quantity_entry = tk.Entry(root)
-quantity_entry.pack()
+# Button for viewing items
+view_button = tk.Button(root, text="View", command=view_items)
+view_button.pack()
 
-remove_quantity_label = tk.Label(root, text="Please enter the quantity to remove:")
-remove_quantity_label.pack()
+# Button for quitting the program
+quit_button = tk.Button(root, text="Quit", command=quit_program)
+quit_button.pack()
 
-remove_quantity_entry = tk.Entry(root)
-remove_quantity_entry.pack()
+root.mainloop()
 
-input_button = tk.Button(root, text="Submit", command=handle_button_click)
-input_button.pack()
-
-def main():
-    root.mainloop()
-
-if __name__ == '__main__':
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    main()
+# if __name__ == '__main__':
+#     # os.system('cls' if os.name == 'nt' else 'clear')
+#     main()
