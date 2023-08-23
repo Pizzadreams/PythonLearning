534af1a38daa147c1f047ecbd51ff92f7158952a
Andrew <retsim120@gmail.com>
2023-08-23 17:58:59 -0400

    update cursor collision code and for pizza to spawn randomly

--- PythonProjects/PizzaGame/main.py
+++ PythonProjects/PizzaGame/main.py
@@ -1,6 +1,7 @@
 import pygame
 from player import Player
 from object import Object
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -13,11 +14,21 @@ window_height = 800
 window = pygame.display.set_mode((window_width, window_height))
 
 pygame.display.set_caption("Pizza Adventure Game")
+pizza_image_path = "assets/Pizza1.png"
+pizza_image = pygame.image.load(pizza_image_path)
 
 # Create instances of the Player and Object classes
-player = Player((window_width / 2, window_height / 2), 5, 2) # 
-pizza = Object((100, 100), "Pizza1.png")
+player = Player((window_width / 2, window_height / 2), 15, 1) # self, position, radius, speed
+# pizza = Object((50, 50), "Pizza1.png")
 
+# Get mask for the pizza image
+pizza_mask = pygame.mask.from_surface(pizza_image)
+
+# Generate random position for the pizza
+pizza_position = (
+    random.randint(0, window_width - pizza_image.get_width()),
+    random.randint(0, window_height - pizza_image.get_height())
+)
 # Game loop
 running = True
 while running:
@@ -31,7 +42,8 @@ while running:
     player.update()
 
     # Check for collision with pizza object
-    if player.rect.colliderect(pizza.rect):
+    pizza_rect = pizza_image.get_rect(topleft=pizza_position)
+    if player.rect.colliderect(pizza_rect):
         # Player has collected the pizza object
         # TODO
         print("Pizza collected!")
@@ -39,7 +51,18 @@ while running:
     # Draw game objects
     window.fill((2, 0, 121))
     player.draw(window)
-    pizza.draw(window)
+    #pizza.draw(window)
+
+    # Draw pizza image
+    window.blit(pizza_image, pizza_position)
+
+# Check for collision with the mouse cursor
+    mouse_pos = pygame.mouse.get_pos()
+    relative_mouse_pos = (mouse_pos[0] - pizza_position[0], mouse_pos[1] - pizza_position[1])
+
+    if 0 <= relative_mouse_pos[0] < pizza_image.get_width() and 0 <= relative_mouse_pos[1] < pizza_image.get_height():
+        if pizza_mask.get_at(relative_mouse_pos):
+            print("Pizza Collision!")
 
     # Update the display
     pygame.display.flip()
