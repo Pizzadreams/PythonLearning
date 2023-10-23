7a13448a38b39f045b5f0edf726788f2eb9a98a1
Andrew <retsim120@gmail.com>
2023-10-23 01:38:30 -0400

    update

--- PythonProjects/Turtle/spyntax.py
+++ PythonProjects/Turtle/spyntax.py
@@ -10,7 +10,7 @@ screen.setup(width=1400, height=1400)
 screen.bgcolor('black')
 
 rainbow_pen = turtle.Turtle()
-rainbow_pen.pensize(4)
+rainbow_pen.pensize(4) 
 rainbow_pen.speed(10)
 colors = ["red", "orange", "yellow", "green", "blue", "blueviolet", "violet"]
 
@@ -18,7 +18,7 @@ for i in range(300):
     rainbow_pen.pencolor(colors[i % len(colors)])
     rainbow_pen.stamp()
     rainbow_pen.forward(i*2)
-    rainbow_pen.left(117)
+    rainbow_pen.left(116)
     rainbow_pen.backward(i*4)
     rainbow_pen.left(64)
     rainbow_pen.forward(i*1)
