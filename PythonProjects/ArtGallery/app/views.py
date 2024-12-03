966796bd54d5df71da7ff81e84a8d68f102938e9
Andrew <retsim120@gmail.com>
2024-12-03 12:30:54 -0500

    update views

--- PythonProjects/ArtGallery/app/views.py
+++ PythonProjects/ArtGallery/app/views.py
@@ -1,19 +1,20 @@
+# gallery/views.py
 import requests
-from django.conf import settings
 from django.shortcuts import render
+from django.conf import settings
+import random
 
-
-def random_image(request):
-    api_key = UNSPLASH_API_KEY
-    url = f"https://api.unsplash.com/photos/random?client_id={api_key}"
-
-    response = request.get(url)
-    data = response.json
-
-    # Check if the response contains an image
-    if 'urls' in data:
-        image_url = data['urls']['regular']
+def random_image_view(request):
+    # Fetch a random image from Unsplash
+    url = f'https://api.unsplash.com/photos/random?client_id={settings.UNSPLASH_ACCESS_KEY}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        artwork = response.json()
+        image_url = artwork['urls']['regular']
+        title = artwork['description'] if artwork['description'] else "Untitled"
     else:
         image_url = None
+        title = "Error fetching image"
 
-    return render(request, 'index.html', {'image_url': image_url})
+    return render(request, 'gallery/index.html', {'image_url': image_url, 'title': title})
