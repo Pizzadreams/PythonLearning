a7beaa8bed4739a3a93747b13d353ea89dcf1c35
Andrew <retsim120@gmail.com>
2025-07-20 20:40:52 -0400

    template views created

--- PythonProjects/art_gallery/gallery/views.py
+++ PythonProjects/art_gallery/gallery/views.py
@@ -1,3 +1,8 @@
+# views are Python functions that take a web request and return a web response
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def gallery(request):
+    return HttpResponse("Hello world!")
