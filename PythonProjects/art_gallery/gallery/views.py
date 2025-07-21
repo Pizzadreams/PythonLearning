9c72cb52b2d4a4e68abd95c8775368518214c0bd
Andrew <retsim120@gmail.com>
2025-07-20 21:48:19 -0400

    updated views and added templates folder with index.html

--- PythonProjects/art_gallery/gallery/views.py
+++ PythonProjects/art_gallery/gallery/views.py
@@ -1,8 +1,8 @@
 # views are Python functions that take a web request and return a web response
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
 
 def gallery(request):
+    template = loader.get_template('gallery/index.html')
     return HttpResponse("Hello world!")
