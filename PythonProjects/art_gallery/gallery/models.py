6290174b47bf3f877e83e93e21757fbd76f70ff1
Andrew <retsim120@gmail.com>
2025-07-22 23:43:25 -0400

    created Model with makemigrations cmd

--- PythonProjects/art_gallery/gallery/models.py
+++ PythonProjects/art_gallery/gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='gallery_images/')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Galleries"
+        ordering = ['title']
