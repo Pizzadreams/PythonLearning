035eacf3e6d4b9d6d71d50fe376b6645ab21d905
Andrew <retsim120@gmail.com>
2025-07-24 01:15:17 -0400

    updated views and index.html to account for random image display on each page load. Passes random # from view to template.

--- PythonProjects/art_gallery/gallery/views.py
+++ PythonProjects/art_gallery/gallery/views.py
@@ -1,8 +1,14 @@
 # views are Python functions that take a web request and return a web response
+import random
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
 def gallery(request):
     template = loader.get_template('index.html')
+    random_number = random.randint(1, 1000)
+    context = {
+        'random_number': random_number,
+    }
     return HttpResponse(template.render())
+
